374aff583c814d2159c48af4777f7fd8a88c9d6c
Neha Kumari <neha.kumari-ext@jci.com>
2023-11-16 13:01:52 +0530

    ediable table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormArray, FormGroup, FormBuilder } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table-form-array';
+
+  displayedColumns: string[] = ['name', 'age', 'food', 'action'];
+  dataSource = new MatTableDataSource();
+  form!: FormGroup;
+  data!: FormArray;
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    // Create form
+    this.form = this.fb.group({
+      data: this.fb.array([])
+    });
+    //add default data
+    this.dateFormArray.push(this.fb.group({ name: ['John'], age: [34], food: ['pizza-1'] }));
+    this.dataSource.data = this.dateFormArray.controls;
+
+  }
+
+// Casting AbstractControl as FormArray
+  get dateFormArray():FormArray {
+    return this.form.get('data') as FormArray;
+  }
+
+
+  addLine(): void {
+    const row = this.fb.group({ name: [], age: [], food: [] });
+    // Add new form in FormArray
+    this.dateFormArray.push(row);
+    // Update table
+    this.dataSource.data = this.dateFormArray.controls;
+  }
+
+  //remove row from table
+  removeLine(index:number){
+    this.dateFormArray.removeAt(index)
+    this.dataSource.data = this.dateFormArray.controls;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CdkTableModule } from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    CdkTableModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
